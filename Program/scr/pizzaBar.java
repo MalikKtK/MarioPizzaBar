dd45c2f3008c2ccb1db8956790fe841fb1144612
MalikKtK <99009682+MalikKtK@users.noreply.github.com>
2022-04-21 11:43:26 +0200

    update

--- Program/scr/pizzaBar.java
+++ Program/scr/pizzaBar.java
@@ -1,7 +1,13 @@
 public class pizzaBar {
     private Menu menu;
+    private userInterface ui;
 
-    public void run(){
+    public void run() {
+        menu.CreatePizza();
+        showMenu();
+    }
 
+    public void showMenu() {
+        ui.printMenu(menu.getPizzaMenu());
     }
 }
--- Program/scr/userInterface.java
+++ Program/scr/userInterface.java
@@ -1,2 +1,11 @@
+import java.util.ArrayList;
+
 public class userInterface {
+
+    public void printString(String message){
+        System.out.println(message);
+    }
+    public void printMenu(ArrayList<Pizza> pizzaMenu){
+        System.out.println(pizzaMenu);
+    }
 }
