7915f02155f9c52dd300be2b549a89f1ec17ac32
MalikKtK <99009682+MalikKtK@users.noreply.github.com>
2022-04-24 18:48:39 +0200

    fixet scanner bug

--- Program/scr/pizzaBar.java
+++ Program/scr/pizzaBar.java
@@ -41,23 +41,6 @@ public class pizzaBar {
 
     public void finishOrder() {
         int id = getValidId();
-        System.out.println("Betaler du med kort eller kontant? (Kort/Kontant)");
-        String payment = ui.getString();
-        switch (payment) {
-            case "kort" -> {
-                System.out.println("Type your pin code");
-                ui.getInt();
-                System.out.println("You have been payed" + order.totalPricePizza() + " Kr.");
-                System.out.println("Have a good day!");
-            }
-            case "kontant" -> {
-                System.out.println("You have to recive" + order.totalPricePizza() + "Kr. ");
-                ui.getInt();
-                System.out.println("You have been payed" + order.totalPricePizza() + " Kr.");
-                System.out.println("Have a good day!");
-            }
-            default -> System.out.println("They dont have enough money");
-        }
         deleteOrder(id);
         System.out.println("Finished order #" + id);
         }
